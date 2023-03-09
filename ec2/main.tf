15899704e5b1dbaabbf44c8fe46748dd58c2b013
Raghu K <raghuk.vit@gmail.com>
2023-03-09 07:47:42 +0530

    Update roboshop-infra/

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "ec2" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   iam_instance_profile   = "${var.env}-${var.component}-role"
   tags = {
-    Name = var.component
+    Name    = var.component
+    Monitor = var.monitor ? "yes" : "no"
   }
 }
 
