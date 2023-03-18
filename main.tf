36e29af143d09ee71c83626d8c4ed978241bb993
Raghu K <raghuk.vit@gmail.com>
2023-03-18 09:02:31 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 module "vpc" {
-  source = "git::https://github.com/raghudevopsb71/tf-module-vpc.git"
-  env    = var.env
-  tags   = var.tags
+  source              = "git::https://github.com/raghudevopsb71/tf-module-vpc.git"
+  env                 = var.env
+  tags                = var.tags
+  default_route_table = var.default_route_table
+  default_vpc_id      = var.default_vpc_id
 
   for_each        = var.vpc
   vpc_cidr        = each.value["vpc_cidr"]
