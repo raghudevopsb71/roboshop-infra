3a904f9b35a80cbf2af72529b61e6aeffb6f7f8d
Raghu K <raghuk.vit@gmail.com>
2023-03-20 09:04:11 +0530

    Update roboshop-infra/

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
 locals {
   ## Private Subnets
   #private_subnet_ids = { for k, v in module.vpc["main"].private_subnets : k => tomap({ "id" = v.id, "availability_zone" = v.availability_zone }) }
-  private_subnet_ids = [for k, v in module.vpc["main"].private_subnets : v.id]
+  #private_subnet_ids = [for k, v in module.vpc["main"].private_subnets : v.id]
+  private_subnet_ids = [for k, v in module.vpc["main"].private_subnets["db-az1"] : v.id]
 }
 
