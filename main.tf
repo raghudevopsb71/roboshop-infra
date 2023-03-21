9cc7d1447ce7c70760bfcdb64dbc22a7b1b594ef
Raghu K <raghuk.vit@gmail.com>
2023-03-21 07:43:43 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -11,20 +11,20 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 }
 
-//module "docdb" {
-//  source              = "git::https://github.com/raghudevopsb71/tf-module-docdb.git"
-//  env                 = var.env
-//  tags                = var.tags
-//
-//  for_each        = var.docdb
-//  engine        = each.value["engine"]
-//  backup_retention_period        = each.value["backup_retention_period"]
-//  preferred_backup_window        = each.value["preferred_backup_window"]
-//  skip_final_snapshot        = each.value["skip_final_snapshot"]
-//  engine_version        = each.value["engine_version"]
-//  subnet_ids
-//
-//}
+module "docdb" {
+  source = "git::https://github.com/raghudevopsb71/tf-module-docdb.git"
+  env    = var.env
+  tags   = var.tags
+
+  for_each                = var.docdb
+  engine                  = each.value["engine"]
+  engine_version          = each.value["engine_version"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  subnet_ids              = local.db_subnet_ids
+
+}
 
 
 output "vpc" {
