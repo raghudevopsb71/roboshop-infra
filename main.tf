f24fbfa42e387d995526097ebd8c364f9b951fce
Raghu K <raghuk.vit@gmail.com>
2023-03-31 08:31:56 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -134,7 +134,7 @@ resource "aws_spot_instance_request" "load-runner" {
   ami                    = data.aws_ami.ami.id
   instance_type          = "t3.medium"
   wait_for_fulfillment   = true
-  vpc_security_group_ids = ["allow-all"]
+  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
 
   tags = merge(
     var.tags,
