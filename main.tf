72a963cd60a343a51c48d46172fb5573057fc34a
Raghu K <raghuk.vit@gmail.com>
2023-04-13 07:41:28 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ module "alb" {
 
 module "app" {
 
-  //depends_on = [module.docdb, module.rds, module.elasticache, module.alb, module.rabbitmq]
+  depends_on = [module.docdb, module.rds, module.elasticache, module.alb, module.rabbitmq]
 
   source           = "git::https://github.com/raghudevopsb71/tf-module-app.git"
   env              = var.env
